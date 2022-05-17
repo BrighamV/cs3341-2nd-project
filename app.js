8a360a519b792aae36ea2ee98a83b25c76f39407
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-05-17 14:57:49 -0600

    connecting to front

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app
     .use(bodyParser.json())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
+        // added next three lines to test frontend
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         next();
     })
     .use('/', require('./routes'));
