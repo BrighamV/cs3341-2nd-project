3dee3245ad9ce44c55790c4f9f0e55ba148b2086
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-05-17 15:20:35 -0600

    keepin on

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         // added next three lines to test frontend
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        // response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-        // response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        // response.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
         next();
     })
     .use('/', require('./routes'));
