f44b4a7cfd564c1f49dc542a187e17b28db0bc95
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-05-20 12:23:28 -0600

    changed to string

--- middleware/validate.js
+++ middleware/validate.js
@@ -3,9 +3,9 @@ const validator = require('../helpers/validate');
 const saveRecipe = (req, res, next) => {
     const validationRule = {
         "name": "required|string",
-        "instructions": "required|array",
-        "ingredients": "required|array",
-        "equipment": "array"
+        "instructions": "required|string",
+        "ingredients": "required|string",
+        "equipment": "string"
     }
     validator(req.body, validationRule, {}, (err, status) => {
         if (!status) {
