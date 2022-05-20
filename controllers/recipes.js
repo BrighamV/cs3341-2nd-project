4dabd9c93489065bff08e4206d018a86dc4bfb4f
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-05-20 13:17:10 -0600

    kraft

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -46,7 +46,7 @@ const addOne = async (req,res,next) => {
         instructions: req.body.instructions,
         ingredients: req.body.ingredients,
         equipment: req.body.equipment,
-        authorName: req.body.equipment
+        authorName: req.body.authorName
     
     }
     const result = await mongodb
