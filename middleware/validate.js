f422b06bd79db9552279267395c616a9144664e2
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-05-20 12:43:43 -0600

    array working

--- middleware/validate.js
+++ middleware/validate.js
@@ -3,8 +3,8 @@ const validator = require('../helpers/validate');
 const saveRecipe = (req, res, next) => {
     const validationRule = {
         "name": "required|string",
-        "instructions": "required|string",
-        "ingredients": "required|string",
+        "instructions": "required|array",
+        "ingredients": "required|array",
         "equipment": "array"
     }
     validator(req.body, validationRule, {}, (err, status) => {
