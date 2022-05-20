c5afba80c39f885fc03fb5ced15f4699fc3493b4
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-05-20 13:39:50 -0600

    int

--- middleware/validate.js
+++ middleware/validate.js
@@ -3,8 +3,8 @@ const validator = require('../helpers/validate');
 const saveRecipe = (req, res, next) => {
     const validationRule = {
         "name": "required|string",
-        "hour": "string",
-        "minute": "string",
+        "hour": "integer",
+        "minute": "integer",
         "instructions": "required|array",
         "ingredients": "required|array",
         "equipment": "array",
