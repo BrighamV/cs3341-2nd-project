e338bf4023337a637dbf68816b8f422795f282b3
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-05-24 14:04:33 -0600

    wow

--- routes/s3Url.js
+++ routes/s3Url.js
@@ -7,8 +7,6 @@ const generate = require('../middleware/s3');
 router.get('/', async (req, res) => {
     const url = await generate.generateUploadURL()
     // console.log("ehre in url", url)
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
     res.send({url})
 })
 
