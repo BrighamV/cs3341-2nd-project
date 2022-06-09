253c855a9f07194a1bc093b5c92121ee0d4b4789
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-06-09 14:11:33 -0600

    oAuth works

--- middleware/passport-setup.js
+++ middleware/passport-setup.js
@@ -1,5 +1,8 @@
 const dotenv = require('dotenv');
 const passport = require('passport');
+const mongodb = require('../db/connect');
+
+
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 dotenv.config();
@@ -12,16 +15,52 @@ module.exports = function(passport){
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: '/auth/google/callback'
 
-        },(acessToken, refreshToken, profile, done) => {
+        },
+        async (acessToken, refreshToken, profile, done) => {
             // passport callback function
             console.log(profile)
+            const newUser = {
+                googleId: profile.id,
+                displayName: profile.displayName,
+                firstname: profile.name.givenName,
+                lastname: profile.name.familyName,
+                image: profile.photos[0].value
+            }
+
+
+            // console.log("user google id", newUser.googleId)
+            // try {
+                let user = await mongodb
+                .getDB()
+                .db()
+                .collection('contacts')
+                .findOne({ googleId: newUser.googleId })
+
+                // console.log("user",user)
+
+                if(user) { 
+                    done(null, user)
+                } else {
+                    let user = await mongodb
+                    .getDB()
+                    .db()
+                    .collection('contacts')
+                    .insertOne(newUser);
+                    
+                    done(null, user)
+                }
+
+            // } catch {
+            //     console.error(err)
+            // }
+
         })
     )
-    passport.serializeUser((user, done) => {
-        done(null, user.id)
-    })
-
-    passport.deserializeUser((id, done) => {
-        User.findById(id, (err, user) =>  done(err, user))
-    })
+    passport.serializeUser(function(user, done) {
+        done(null, user);
+    });
+      
+    passport.deserializeUser(function(user, done) {
+        done(null, user);
+    });
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,39 @@
 const express = require('express')
 const router = require('express').Router();
 const passport = require('passport');
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 // auth login
 router.get('/login',(req, res) => {
     res.render('login');
 });
 
-// auth logout
-router.get('/logout', (req, res) => {
-    //handle with passport
-    res.send('logging out')
-})
 
 // auth with google
-router.get('/google', passport.authenticate('google', { scope: ['profile']}), (req, res) => {
+router.get('/google', ensureGuest, passport.authenticate('google', { scope: ['profile']}), (req, res) => {
     res.send("send it")
 });
 
 // auth with google callback
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/'}),
  (req, res) => {
-     res.redirect('/search')
+     res.redirect('http://localhost:4200/')
  });
 
+// auth logout
+// router.get('/logout', (req, res) => {
+//     //handle with passport
+//     // res.send('logging out')
+//     req.logout()
+//     res.redirect('http://localhost:4200/')
+// })
+
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('http://localhost:4200/');
+    });
+  });
 
 module.exports = router;
